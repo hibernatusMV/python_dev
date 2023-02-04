357c6ea3c08a1d0c7c43d71d082946765b7cb21c
Marcus <40230046+hibernatusMV@users.noreply.github.com>
2023-02-04 22:13:56 +0100

    code to show selective columns

--- observationlog/classes/database.py
+++ observationlog/classes/database.py
@@ -1,14 +1,39 @@
-""" Class for database transctions """
+""" Class for database transactions """
 import sqlite3
-import os
 
 class database:
     """ connection to database """
-    data = ""
+    data = {} # initialize array
+    rowcount = 0
 
     def execQuery(query):
-        # print(os.getcwd())
         with sqlite3.connect("database/observationlog.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute(query)
-            database.data = cursor.fetchall()
+            try:
+                cursor = conn.cursor()
+                cursor.execute(query)
+            
+                row = 0
+                col = 0
+            
+                for dataset in cursor:
+                    for value in dataset:
+                        database.data[row,col] = value
+                        col += 1
+                    row += 1
+            
+            except sqlite3.OperationalError:
+                print("Tabelle nicht gefunden!")
+            except:
+                print("Daten konnten nicht verarbeitet werden!")
+
+    def rowcount(query):
+        with sqlite3.connect("database/observationlog.db") as conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(query)
+                database.rowcount = cursor.fetchone()
+            
+            except sqlite3.OperationalError:
+                print("Tabelle nicht gefunden!")
+            except:
+                print("Daten konnten nicht verarbeitet werden!")
--- observationlog/obslog.py
+++ observationlog/obslog.py
@@ -1,4 +1,10 @@
 from classes.database import database
 
 database.execQuery("select * from observations")
-print(database.data)
+database.rowcount("select count(*) from observations")
+i = 0
+row = database.rowcount[0] - 1
+
+while i <= row:
+    print("Objekt: ", database.data[i,3], "Sternbild: ", database.data[i,7])
+    i += 1
